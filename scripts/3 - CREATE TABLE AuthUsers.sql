684bda097c471f8e6fa33ec8e4895a878e7ec61c
WaineAlvesCarneiro <carneirowaine@gmail.com>
2026-01-29 17:50:11 -0300

    Criando endpoint Usuários

--- scripts/3 - CREATE TABLE AuthUsers.sql
+++ scripts/3 - CREATE TABLE AuthUsers.sql
@@ -1,12 +1,13 @@
 IF OBJECT_ID('[dbo].[AuthUsers]', 'U') IS NULL
 BEGIN
     CREATE TABLE [dbo].[AuthUsers] (
-        [Id]           UNIQUEIDENTIFIER CONSTRAINT PK_AuthUsers PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
-        [EmpresaId]    BIGINT NULL,
-        [UserName]     VARCHAR(100)     NOT NULL CONSTRAINT UQ_AuthUsers_UserName UNIQUE,
-        [PasswordHash] NVARCHAR(MAX)    NOT NULL,
-        [Role]         VARCHAR(100)      NOT NULL,
-        [Dt_Criacao]   DATETIMEOFFSET   NOT NULL CONSTRAINT DF_AuthUsers_Dt_Criacao DEFAULT SYSDATETIMEOFFSET()
+        [Id]             UNIQUEIDENTIFIER CONSTRAINT PK_AuthUsers PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+        [EmpresaId]      BIGINT NULL,
+        [UserName]       VARCHAR(100)     NOT NULL CONSTRAINT UQ_AuthUsers_UserName UNIQUE,
+        [PasswordHash]   NVARCHAR(MAX)    NOT NULL,
+        [Role]           VARCHAR(100)      NOT NULL,
+        [DataInclusao]   DATETIME2 NOT NULL CONSTRAINT DF_AuthUsers_Inclusao DEFAULT GETDATE(),
+        [DataAlteracao]  DATETIME2 NULL,
     );
 END
 GO
