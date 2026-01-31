4f46b8eb5fe1597e4c4c0abdd8ee80ac488862ed
WaineAlvesCarneiro <carneirowaine@gmail.com>
2026-01-31 16:42:40 -0300

    Primeiro acesso do usuário força a alterar a senha

--- scripts/3 - CREATE TABLE AuthUsers.sql
+++ scripts/3 - CREATE TABLE AuthUsers.sql
@@ -1,13 +1,15 @@
 IF OBJECT_ID('[dbo].[AuthUsers]', 'U') IS NULL
 BEGIN
     CREATE TABLE [dbo].[AuthUsers] (
-        [Id]             UNIQUEIDENTIFIER CONSTRAINT PK_AuthUsers PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
-        [EmpresaId]      BIGINT NULL,
-        [UserName]       VARCHAR(100)     NOT NULL CONSTRAINT UQ_AuthUsers_UserName UNIQUE,
-        [PasswordHash]   NVARCHAR(MAX)    NOT NULL,
-        [Role]           VARCHAR(100)      NOT NULL,
-        [DataInclusao]   DATETIME2 NOT NULL CONSTRAINT DF_AuthUsers_Inclusao DEFAULT GETDATE(),
-        [DataAlteracao]  DATETIME2 NULL,
+        [Id]              UNIQUEIDENTIFIER CONSTRAINT PK_AuthUsers PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+        [EmpresaId]       BIGINT NULL,
+        [UserName]        VARCHAR(100) NOT NULL CONSTRAINT UQ_AuthUsers_UserName UNIQUE,
+        [Email]           VARCHAR(100) NOT NULL,
+        [PasswordHash]    NVARCHAR(MAX) NOT NULL,
+        [Role]            VARCHAR(100) NOT NULL,
+        [PrimeiroAcesso]  BIT NOT NULL,
+        [DataInclusao]    DATETIME2 NOT NULL CONSTRAINT DF_AuthUsers_Inclusao DEFAULT GETDATE(),
+        [DataAlteracao]   DATETIME2 NULL,
     );
 END
 GO
